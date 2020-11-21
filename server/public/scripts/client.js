b7fe411ff6da4f220c340769dbb94738e582113b
John Micko <jrmicko5012@gmail.com>
2020-11-21 16:08:41 -0600

    remove focus after add to-do button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,16 @@ $(document).ready(onLoad);
 
 function onLoad() {
     console.log('jq');
+  addClickHandlers();
+
+}
+
+function addClickHandlers() {
+    $('.new-to-do-buttons').on('click', addTodo)
+}
+
+function addTodo(event) {
+    event.preventDefault();
+    this.blur();
+    console.log('clicked add to-do button');
 }
