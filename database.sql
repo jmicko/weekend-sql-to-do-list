4de2d0547969980a8f8cd814f83d1da7cd7b4e7a
John Micko <jrmicko5012@gmail.com>
2020-11-23 11:33:48 -0600

    add npm start was using nodemon and forgot to add

--- database.sql
+++ database.sql
@@ -9,12 +9,3 @@ CREATE TABLE todos(
 INSERT INTO "todos" ("task") VALUES ('Take the bacon away from the dog. If he does not like that idea, send him to whole foods and show him the vegetable bacon.');
 INSERT INTO "todos" ("task") VALUES ('Put a lemon back in the waffle maker');
 INSERT INTO "todos" ("task", "status") VALUES ('Buy 2 pounds of fresh ground beef', 'complete');
-
-SELECT * FROM "todos";
-SELECT * FROM "todos" WHERE status='incomplete' ORDER BY status;
-SELECT * FROM "todos" ORDER BY created_at DESC;
-SELECT * FROM "todos" WHERE status='incomplete' ORDER BY created_at DESC;
-SELECT * FROM "todos" ORDER BY status;
-SELECT * FROM "todos" WHERE status='incomplete' ORDER BY completed_at DESC;
-
--- DELETE FROM todos WHERE id=2;
