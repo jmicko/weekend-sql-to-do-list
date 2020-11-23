fa0c0659e4a1bdf1d0133d361fed139253c21584
John Micko <jrmicko5012@gmail.com>
2020-11-23 11:41:26 -0600

    change pool settings for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,30 @@
 const pg = require('pg');
 
 // Set up our pool
-const config = {
-  database: 'weekend-to-do-app', 
-  host: 'localhost', 
-  port: 5432, 
-  max: 10, 
-  idleTimeoutMillis: 30000 
-};
+const Pool = pg.Pool;
+
+// make our own instance of a Pool from that template Pool object
+
+let config ={}
+
+if ( process.env.DATABASE_URL ) {
+  // running remote (heroku)
+  config= {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthenticated: false}
+  }
+} else {
+  // running locally
+  config = {
+    database: 'weekend-to-do-app', 
+    host: 'localhost', 
+    port: 5432, 
+    max: 10, 
+    idleTimeoutMillis: 30000 
+  };
+
+}
+
 
 const pool = new pg.Pool(config);
 
